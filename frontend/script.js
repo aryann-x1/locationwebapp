8f63ecdf01eb48bd86cd2e0f63b3441d361f9c83
[aryan] <[aryanarora3011@gmail.com]>
2025-03-22 15:11:05 +0530

    Fixed location update issue, added names to markers

--- frontend/script.js
+++ frontend/script.js
@@ -26,8 +26,10 @@ function getUserColor(userId) {
 
 // Function to create a custom marker icon
 function createCustomIcon(color) {
+  const iconUrl = `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${color}.png`;
+
   return L.icon({
-    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${color}.png`,
+    iconUrl: iconUrl || 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
@@ -35,6 +37,7 @@ function createCustomIcon(color) {
     shadowSize: [41, 41],
   });
 }
+
 
 socket.onmessage = async (event) => {
   try {
